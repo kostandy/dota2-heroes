de2f3101d653fcb6ec7bcb858aa4a9def5f088df
Dmitry <spaser15@live.ru>
2018-08-14 17:02:33 +0300

    Added ability icons to files. Fixed some styles. Displayed hero list.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import Navbar from './navbar';
 import './App.css';
 
+const importAllHeroImages = r => r.keys().map(r);
+
+const images = importAllHeroImages(require.context('./icon/heroes', false, /\.png$/));
+
 class App extends Component {
 
 	constructor(props) {
@@ -23,9 +27,14 @@ class App extends Component {
 
 			this.setState({
 				isLoaded: true,
-				items: data
+				items: data.sort((a, b) => {
+					var keyA = a.localized_name,
+							keyB = b.localized_name;
+					if (keyA < keyB) return -1;
+					if (keyA > keyB) return 1;
+					return 0;
+				})
 			});
-
 		} catch (error) {
 			this.setState({
 				isLoaded: true,
@@ -36,6 +45,12 @@ class App extends Component {
 
 	render() {
 		const { error, isLoaded, items } = this.state;
+
+		const heroes = 'heroes';
+		const heroPreview = 'hero-preview';
+		const heroName = 'hero-name'
+		const heroRoles = 'hero-roles';
+
 		if (error) {
 			return <div>Error: {error.message}</div>;
 		} else if (!isLoaded) {
@@ -48,11 +63,18 @@ class App extends Component {
 					<Navbar />
 
 					<main>
-						Result:
-						<ul>
-							{items && items.length && items.map(item => (
+						<ul className={heroes}>
+							{items.map((item, i) => (
 								<li key={item.id}>
-									{item.localized_name}
+									<div className={heroPreview}>
+										<img src={images[i]} alt={item.localized_name} width={240} height={136} />
+										<p className={heroName}>{item.localized_name}</p>
+									</div>
+									<div className={heroRoles}>
+										<ul>
+											{item.roles.map((role, i) => ( <li key={i}>{role}</li> )) }
+										</ul>
+									</div>
 								</li>
 							))}
 						</ul>
