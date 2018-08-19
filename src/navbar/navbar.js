3a1159d0b49b104edb08ce1383b726f7a22b99bc
Dmitry <spaser15@live.ru>
2018-08-19 14:01:48 +0300

    Added full logo

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 
-import logo from './logo.png'
+import logo from './logo-full.png'
 
 import './navbar.css'
 
 class Navbar extends Component {
 
 	render() {
+
+		const classLogo = 'logo'
+
 		return <nav>
-			<img src={logo} alt="Dota 2" width="75px" height="auto" />
+			<img src={logo} alt="Dota 2" width="auto" height="65px" className={classLogo} />
 		</nav>
 	}
 
