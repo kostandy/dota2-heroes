adda78f86d3c2881fa7b4f9d806f97d06816e806
Dmitry <spaser15@live.ru>
2018-08-27 23:09:49 +0300

    Updated footer info

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { Container, Grid } from 'semantic-ui-react'
+import { Container, Grid, Loader, Dimmer, Comment} from 'semantic-ui-react'
 
 import './Footer.css'
 
@@ -8,25 +8,89 @@ export default class Footer extends Component {
 
 	constructor (props) {
 		super(props)
+		this.state = {
+			error: null,
+			isLoaded: false,
+			author: null
+		}
+	}
 
+	async componentDidMount () {
+
+		const author = 'Kostandy'
+
+		try {
+
+			const res = await fetch(`https://api.github.com/users/${author}`)
+
+			let data = await res.json()
+
+			this.setState({
+				isLoaded: true,
+				author: data
+			})
+		} catch (error) {
+			this.setState({
+				isLoaded: true,
+				error
+			})
+		}
 	}
 
 	render () {
 
+		const {error, isLoaded, author} = this.state
+
 		const classMainFooter = 'main-footer'
 
-		return <footer className={classMainFooter}>
-			<Container>
-				<Grid>
-					<Grid.Column floated='left' width={5}>
-						<Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-					</Grid.Column>
-					<Grid.Column floated='right' width={5}>
-						<Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-					</Grid.Column>
-				</Grid>
-			</Container>
-		</footer>
+		if (error) {
+
+			console.error(error)
+
+		} else {
+
+			return <footer className={classMainFooter}>
+
+				<Container>
+
+					{isLoaded && <Grid>
+							<Grid.Column floated='left' width={5}>
+
+								<Comment.Group>
+									<Comment>
+										<Comment.Avatar src={author.avatar_url} size='small'/>
+										<Comment.Content>
+											Created by &nbsp;
+											<Comment.Author as='a' href={`https://github.com/${author.login}`} target="_blank"
+											                title={author.login}>
+												{author.login}
+											</Comment.Author>
+											<Comment.Text>
+												Thank you for visit ;)
+											</Comment.Text>
+										</Comment.Content>
+									</Comment>
+								</Comment.Group>
+
+							</Grid.Column>
+
+							<Grid.Column floated='right' align="right" width={5}>
+								Powered by <a href="https://netlify.com" target="_blank" title="Netlify">Netlify</a>
+							</Grid.Column>
+
+						</Grid>
+					}
+
+					{!isLoaded && <Dimmer active>
+							<Loader />
+						</Dimmer>
+					}
+
+				</Container>
+
+			</footer>
+
+		}
 	}
 
 };
