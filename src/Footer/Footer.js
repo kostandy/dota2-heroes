8ff38f50386d9f327bd3b36f57cf735043137946
Dmitry <spaser15@live.ru>
2018-08-28 18:09:44 +0300

    Updated footer author caption

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { Container, Grid, Comment} from 'semantic-ui-react'
+import { Container, Grid, Label, Image } from 'semantic-ui-react'
 
 import './Footer.css'
 
@@ -18,34 +18,22 @@ export default class Footer extends Component {
 
 					<Grid.Column>
 
-						<Comment.Group>
-
-							<Comment>
-
-								<Comment.Avatar src="https://avatars2.githubusercontent.com/u/12787555?v=4" size='small'/>
-
-								<Comment.Content>
-
-									Created by &nbsp;
-
-									<Comment.Author as='a' href='https://github.com/Kostandy' target="_blank" rel="noopener noreferrer"
-									                title="Kostandy">Kostandy</Comment.Author>
-
-									<Comment.Text>
-										Thank you for visit ;)
-									</Comment.Text>
-
-								</Comment.Content>
-
-							</Comment>
-
-						</Comment.Group>
+						<Label as='a' href='https://github.com/Kostandy' target="_blank" rel="noopener noreferrer"
+						       title="Kostandy" color="grey">
+							<Image avatar spaced='right' src='https://avatars2.githubusercontent.com/u/12787555?v=4' />
+							Kostandy
+						</Label>
 
 					</Grid.Column>
 
 					<Grid.Column align="right" width={8}>
-						Powered by <a href="https://netlify.com" target="_blank" rel="noopener noreferrer"
-						              title="Netlify">Netlify</a>
+
+						<Label as='a' href='https://netlify.com' target="_blank" rel="noopener noreferrer"
+						       title="Netlify" color="grey">
+							<Image avatar spaced='right' src='https://www.netlify.com/img/press/logos/logomark.png' />
+							Powered by <b>Netlify</b>
+						</Label>
+
 					</Grid.Column>
 
 				</Grid>
